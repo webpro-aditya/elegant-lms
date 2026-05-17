b8ce5689239a168844f7fc663675ba7cc11ff898
Aditya D <webpro.aditya@gmail.com>
2026-05-17 17:47:12 +0530

    mdy: privacy policy design issue

--- Modules/AoraPageBuilder/Resources/views/pages/show.blade.php
+++ Modules/AoraPageBuilder/Resources/views/pages/show.blade.php
@@ -2,6 +2,53 @@
 @section('og_image'){{asset(Settings('logo'))}}@endsection
 
 @section('content')
+    <style>
+        /* Premium Spacing & Padding for Pagebuilder Content */
+        #content-area {
+            padding-bottom: 60px; /* Elegant bottom padding before footer */
+        }
+        
+        #content-area .row {
+            margin-left: 0 !important;
+            margin-right: 0 !important;
+        }
+        
+        #content-area [class*="col-"] {
+            padding-left: 48px !important;
+            padding-right: 48px !important;
+        }
+        
+        /* Make sure inner elements have nice line height and spacing for readability */
+        #content-area p {
+            line-height: 1.8 !important;
+            margin-bottom: 20px !important;
+            font-size: 15.5px !important;
+            color: #4b5563 !important; /* Premium slate-grey color */
+        }
+        
+        #content-area h1, 
+        #content-area h2, 
+        #content-area h3, 
+        #content-area h4, 
+        #content-area h5, 
+        #content-area h6 {
+            margin-top: 35px !important;
+            margin-bottom: 18px !important;
+            font-weight: 700 !important;
+            color: #1f2937 !important;
+        }
+
+        @media (max-width: 767px) {
+            #content-area [class*="col-"] {
+                padding-left: 20px !important;
+                padding-right: 20px !important;
+            }
+            #content-area {
+                padding-bottom: 40px;
+            }
+        }
+    </style>
+
     @php
     $url =config('app.short_url').'/public';
     if (!config('app.has_public_folder')){
